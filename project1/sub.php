d028b0d53eb846ce83da91833fb1ecd303ffd5c3
Ubuntu <ubuntu@ip-172-31-94-150.ec2.internal>
2023-09-29 12:56:58 +0000

    Initial commit

---
+++ project1/sub.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+   echo "Hello Amiya It's running because the we have created /var/www/html/ file inside the container on the host Machine. Happy Learning." ;
+?>
+
+</body>
+</html>
